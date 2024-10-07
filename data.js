f5d2cb16fe7daa75d202ccc3845614ffd0e9e33f
issam <issamcydia@gmail.com>
2024-10-07 13:48:00 +0100

    data

---
+++ data.js
@@ -0,0 +1,73 @@
+const books = [
+    {
+      title: "Black Swan",
+      author: "67025ba971cf9c880ae6a814",
+      description: "About Black Swan",
+      price: 10,
+      cover: "soft cover",
+    },
+    {
+      title: "Skin In The Game",
+      author: "67025ba971cf9c880ae6a814",
+      description: "About Skin In The Game",
+      price: 12,
+      cover: "soft cover",
+    },
+    {
+      title: "Fooled By Randomness",
+      author: "67025ba971cf9c880ae6a814",
+      description: "About Fooled By Randomness",
+      price: 8,
+      cover: "hard cover",
+    },
+    {
+      title: "The Forty Rules Of Loves",
+      author: "67025bf471cf9c880ae6a816",
+      description: "About The Forty Rules Of Loves",
+      price: 14,
+      cover: "soft cover",
+    },
+    {
+      title: "The Island Of Missing Trees",
+      author: "67025bf471cf9c880ae6a816",
+      description: "About The Island Of Missing Trees",
+      price: 9,
+      cover: "soft cover",
+    },
+    {
+      title: "The Flea Palace",
+      author: "67025bf471cf9c880ae6a816",
+      description: "About The Flea Palace",
+      price: 10,
+      cover: "hard cover",
+    },
+  ];
+  
+  const authors = [
+    {
+      firstname: "Robert",
+      age: 17,
+      nationality: "USA",
+    },
+    {
+      firstname: "Paulo",
+      age: 45,
+      nationality: "Brazil",
+    },
+    {
+      firstname: "Wayne",
+      age: 33,
+      nationality: "USA",
+    },
+    {
+      firstname: "Rolf",
+      age: 25,
+      nationality: "Switzerland",
+    },
+  ];
+  
+  
+  
+  module.exports = {
+    books,authors
+  };
